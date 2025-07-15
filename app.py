eca9638a54acf96fdb8b3c9b37545cfd382916d5
PRITHIV SAKTHI U R <117709109+PRITHIVSAKTHIUR@users.noreply.github.com>
2025-07-15 22:00:03 +0530

    Update 15 JUL '25

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.title(' Text from Image')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     st.write("")
     st.write("Hold tight..")
     extracted_text, confidence = extract_text(image, 'en')
